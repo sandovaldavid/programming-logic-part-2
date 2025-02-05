67e13f75c35cebc81e70d69fee187188b1e2f64e
sandovaldavid <sandovaldavid2201@gmail.com>
2025-02-04 22:33:54 -0500

    ✨ add multiple functions for console output and calculations in desafio.js

---
+++ desafio/02/desafio.js
@@ -0,0 +1,41 @@
+// # 01
+function message() {
+    console.log("¡Hola, mundo!");
+}
+
+message();
+
+// # 02
+function customizeMessage(name) {
+    console.log(`¡Hola, ${name}!`);
+}
+
+customizeMessage("David");
+
+// # 03
+function doubleNumber(number) {
+    return number * 2;
+}
+
+doubleNumber(5);
+
+// # 04
+function average(a, b, c) {
+    return (a + b + c) / 3;
+}
+
+average(1, 2, 3);
+
+// # 05
+function isLargest(x, y) {
+    return x > y ? x : y;
+}
+
+console.log(isLargest(5, 10));
+
+// # 06
+function squareNumber(number) {
+    return number * number;
+}
+
+squareNumber(5);
