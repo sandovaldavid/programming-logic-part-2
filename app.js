f203b9c9a6876227427c4c3da465721ea9ca2842
sandovaldavid <sandovaldavid2201@gmail.com>
2025-02-04 22:17:42 -0500

    ✨ implement user intent tracking with attempts and update game feedback

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 let endNumber = 10;
+let intentNumber = 5;
+let count = intentNumber;
 let secretNumber = generateRandomNumber();
 
 function assignTextElement(title, label) {
@@ -10,7 +12,44 @@ assignTextElement("Juego del Número Secreto !", "h1");
 assignTextElement("Adivina el número secreto entre 1 y 10", "P");
 
 function userIntent() {
-    alert("Has hecho click en el botón");
+    let userNumber = document.getElementById("userNumber").value;
+
+    if (userNumber < 1 || userNumber > 10) {
+        alert("Por favor, ingrese un número entre 1 y 10");
+    } else {
+        if (userNumber == secretNumber) {
+            winGame(count);
+        } else {
+            count--;
+            if (count == 0) {
+                loseGame();
+            }
+            if (userNumber < secretNumber) {
+                assignTextElement("El número secreto es mayor", "p");
+            } else {
+                assignTextElement("El número secreto es menor", "p");
+            }
+
+            assignTextElement("Te quedan " + count + " intentos", "span");
+        }
+    }
+}
+
+function loseGame() {
+    assignTextElement(
+        "¡Perdiste! El número secreto era: " + secretNumber,
+        "h1"
+    );
+    assignTextElement("¡Superaste el número maximo de intentos!", "p");
+}
+
+function winGame(count) {
+    assignTextElement("¡Ganaste! El número secreto era: " + secretNumber, "h1");
+    assignTextElement("¡Felicidades!", "p");
+    assignTextElement(
+        "¡Adivinaste	el número en " + (intentNumber - count) + " intentos!",
+        "span"
+    );
 }
 
 function generateRandomNumber() {
