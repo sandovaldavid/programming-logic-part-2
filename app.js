a93a8883649270484eecbf4f5e34f97b24446c79
sandovaldavid <sandovaldavid2201@gmail.com>
2025-02-04 20:00:41 -0500

    ✨ add initial game title and instructions

---
+++ app.js
@@ -0,0 +1,5 @@
+let title = document.querySelector('h1');
+title.innerHTML = "Juego del Número Secreto !";
+
+let phrase = document.querySelector('p');
+phrase.innerHTML = "Adivina el número secreto entre 1 y 10";
